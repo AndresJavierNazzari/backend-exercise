5a1cdfa2581e103b2df626a762a3c6130beb97f2
Andres Nazzari <lordcliff@gmail.com>
2023-11-24 01:22:41 +0900

    Update and finish class 3 InputCommands program

--- class3/InputCommands/InputCommands/Program.cs
+++ class3/InputCommands/InputCommands/Program.cs
@@ -18,9 +18,11 @@ internal class Program
             {
                 case '1':
                     Console.Clear();
+                    InputCommands();
                     break;
                 case '2':
                     Console.Clear();
+                    ShowCategories();
                     break;
                 case '3':
                     Console.Clear();
@@ -47,7 +49,54 @@ internal class Program
 
     static char GetChoice()
     {
-        Console.Write("Enter your choice (1-3): ");
-        return Console.ReadKey().KeyChar;
+        char choice;
+        while (!char.TryParse(Console.ReadKey().KeyChar.ToString(), out choice) || (choice < '1' || choice > '3'))
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+            Console.Write("Enter your choice (1-3): ");
+        }
+
+        return choice;
+    }
+
+    static void InputCommands()
+    {
+        Console.Write("Enter commands (text or numbers): ");
+        string? userInput = Console.ReadLine();
+
+        while (string.IsNullOrEmpty(userInput))
+        {
+            Console.WriteLine("Invalid input. Please enter a non-empty value.");
+            userInput = Console.ReadLine();
+        }
+
+        CategorizeInput(userInput.Trim());
+    }
+
+    static void CategorizeInput(string userInput)
+    {
+        if (int.TryParse(userInput, out int numericValue))
+        {
+            numericCommands.Add(numericValue);
+        }
+        else
+        {
+            textCommands.Add(userInput);
+        }
+    }
+
+    static void ShowCategories()
+    {
+        Console.WriteLine("\nText Commands:");
+        foreach (var textCommand in textCommands)
+        {
+            Console.WriteLine($"- {textCommand}");
+        }
+
+        Console.WriteLine("\nNumeric Commands:");
+        foreach (var numericCommand in numericCommands)
+        {
+            Console.WriteLine($"- {numericCommand}");
+        }
     }
 }
--- class3/README.md
+++ class3/README.md
@@ -7,3 +7,7 @@ Create a console application that gives the user 3 options:
 
 When selecting 1 the user can input anything, this information shoud be categorized between text and numbers, and separated in different collections.
 Whent selecting 2 the user should see a List of Text commands and a List of numeric commands
+
+### Result
+
+![Alt text](image.png)
