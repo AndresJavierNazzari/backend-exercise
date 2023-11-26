3d53940fe167272194bfa7ee488ebed8e2b35489
Andres Nazzari <andresnazzari@gmail.com>
2023-11-26 15:54:32 +0900

    Create Quiz class

---
+++ class5/QuizzApp/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzApp
+{
+    internal class Quiz
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string name , string description, List<Question> questions ) {
+            this.Name = name;
+            this.Description = description;
+            this.Questions = questions;
+        }
+    }
+}
