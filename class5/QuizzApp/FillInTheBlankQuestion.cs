86790a063756986c477075c204e31e752122735a
Andres Nazzari <andresnazzari@gmail.com>
2023-11-26 15:54:21 +0900

    Create FillInTheBlankQuestion class

---
+++ class5/QuizzApp/FillInTheBlankQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzApp
+{
+    internal class FillInTheBlankQuestion : Question
+    {
+        public string Answer {  get; set; }
+
+        public FillInTheBlankQuestion(string questionText, string answer, int score) : base(questionText, score) {
+            this.Answer = answer;
+         }
+    }
+}
