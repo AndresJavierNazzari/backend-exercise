eba4e5d148f95d9c1fe028514d8a057fdb3100a3
Andres Nazzari <andresnazzari@gmail.com>
2023-11-26 15:54:00 +0900

    Create Question class

---
+++ class5/QuizzApp/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizzApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public int Score { get; set; }
+
+        public Question(string questionText, int score) { 
+            this.QuestionText = questionText;
+            this.Score = score;
+        }
+
+    }
+}
