55fb094af6e944af2f0d41bd47dea24ce5f89412
Andres Nazzari <andresnazzari@gmail.com>
2023-11-26 17:09:06 +0900

    Create program to test game

--- class5/QuizzApp/Program.cs
+++ class5/QuizzApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QuizzApp;
+class Program
+{
+    static void Main()
+    {
+        // Crear instancias de preguntas
+        MultipleChoiceQuestion question1 = new MultipleChoiceQuestion("What is the capital of France?", new List<string> { "London", "Paris", "Berlin" }, 2, 10);
+        FillInTheBlankQuestion question2 = new FillInTheBlankQuestion("The largest planet in our solar system is __.", "Jupiter", 15);
+
+        // Crear instancias de un cuestionario
+        List<Question> questions = new List<Question> { question1, question2 };
+        Quiz quiz = new Quiz("General Knowledge", "A quiz to test your general knowledge", questions);
+        Quiz quiz2 = new Quiz("General Planets", "A quiz to test your general planets", questions);
+
+        List<Quiz> quizList = [quiz, quizs2];
+
+        // Crear una instancia de Game y comenzar el juego
+        Game game = new Game();
+        game.SelectQuiz(quizList);
+    }
+}
