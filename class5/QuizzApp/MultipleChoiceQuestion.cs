19b8bf5e4d932a7aa33e576a97ccbf45b20874c6
Andres Nazzari <andresnazzari@gmail.com>
2023-11-26 15:54:11 +0900

    Create MultipleChoiceQuestion

---
+++ class5/QuizzApp/MultipleChoiceQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzApp
+{
+    internal class MultipleChoiceQuestion : Question
+    {
+        public required List<string> Choices {  get; set; }
+        public int Answer { get; set; }
+
+        public MultipleChoiceQuestion(string questionText, int score, List<string> choices, int answer) : base (questionText, score)
+        {
+            this.Choices = choices;
+            this.Answer = answer;
+        }
+    }
+}
