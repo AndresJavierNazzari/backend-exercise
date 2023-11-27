3bcc5cdefa6303e3c10f8e3e7f0bdecbfce2c631
Andres Nazzari <lordcliff@gmail.com>
2023-11-27 13:28:40 +0900

    Create method to create question in FillInTheBlankQuestion class

--- class5/QuizzApp/FillInTheBlankQuestion.cs
+++ class5/QuizzApp/FillInTheBlankQuestion.cs
@@ -2,10 +2,27 @@
 {
     internal class FillInTheBlankQuestion : Question
     {
-        public string Answer {  get; set; }
+        public string Answer { get; set; }
 
-        public FillInTheBlankQuestion(string questionText, string answer, int score) : base(questionText, score) {
+        public FillInTheBlankQuestion(string questionText, string answer, int score) : base(questionText, score)
+        {
             this.Answer = answer;
-         }
+        }
+
+        public static FillInTheBlankQuestion CreateQuestion()
+        {
+            string questionText = Menus.EnterProperty("question", "text");
+            string questionAnswer = Menus.EnterProperty("question", "answer");
+            string questionScoreStr = Menus.EnterProperty("question", "score");
+            int questionScore;
+
+            while(!int.TryParse(questionScoreStr, out questionScore))
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+                questionScoreStr = Menus.EnterProperty("question", "score");
+            }
+
+            return new FillInTheBlankQuestion(questionText, questionAnswer, questionScore);
+        }
     }
 }
