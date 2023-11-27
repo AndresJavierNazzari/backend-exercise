9a82ba18dc77d66412c3ca03938be472889d4704
Andres Nazzari <lordcliff@gmail.com>
2023-11-28 07:18:29 +0900

    Create Car class

---
+++ class6/VehicleManagement/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VehicleManagement
+{
+    internal class Car : Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine($"The {nameof(Car)} {Brand} {Model} is starting...");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine($"The {nameof(Car)} {Brand} {Model} is stopping");
+        }
+    }
+}
