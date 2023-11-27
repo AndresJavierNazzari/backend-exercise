3e94a9d4bc2673a71eb568f592e0b0c7da139f5c
Andres Nazzari <lordcliff@gmail.com>
2023-11-28 07:18:39 +0900

    Create Motorcicle class

---
+++ class6/VehicleManagement/Motorcycle.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleManagement
+{
+    internal class Motorcycle : Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine($"The {nameof(Motorcycle)} {Brand} {Model} is starting...");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine($"The {nameof(Motorcycle)} {Brand} {Model} is stopping...");
+        }
+    }
+}
