d737f36bed703f575d177053b70f6e7ff775e678
Andres Nazzari <lordcliff@gmail.com>
2023-11-28 07:18:23 +0900

    Create vehicle class

---
+++ class6/VehicleManagement/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleManagement
+{
+    internal abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public abstract void Start();
+        public abstract void Stop();
+
+
+    }
+}
