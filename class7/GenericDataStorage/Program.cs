f362a6f2e47ee5ed42cf7556e7336951719755c6
Andres Nazzari <lordcliff@gmail.com>
2023-11-29 07:13:05 +0900

    Add program

--- class7/GenericDataStorage/Program.cs
+++ class7/GenericDataStorage/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GenericDataStorage;
+
 Console.WriteLine("Hello, World!");
+
+DataStorage<Person> personStorage = new DataStorage<Person>();
+Person person2 = new Person("Andres2", 36);
+
+personStorage.Add(new Person("Andres", 36));
+personStorage.Add(person2);
+
+
+Console.WriteLine(DataStorage<Person>.TotalItems);
+
