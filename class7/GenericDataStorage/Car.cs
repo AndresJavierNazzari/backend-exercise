18e5cc42cf8902c15c099f315e6ead812cad74b3
Andres Nazzari <lordcliff@gmail.com>
2023-11-29 18:24:34 +0900

    Add Car class and test DataStorage with the second class

---
+++ class7/GenericDataStorage/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericDataStorage
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Car(string brand, string model)
+        {
+            this.Brand = brand;
+            this.Model = model;
+        }
+        public Car()
+        {
+
+        }
+        public void Start()
+        {
+            Console.WriteLine($"The {nameof(Car)} {Brand} {Model} is starting...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"The {nameof(Car)} {Brand} {Model} is stopping");
+        }
+    }
+}
--- class7/GenericDataStorage/Program.cs
+++ class7/GenericDataStorage/Program.cs
@@ -3,12 +3,22 @@ using GenericDataStorage;
 
 Console.WriteLine("Hello, World!");
 
-DataStorage<Person> personStorage = new DataStorage<Person>();
-Person person2 = new Person("Andres2", 36);
+DataStorage<Person> personStorage = new();
+Person person = new("Andres", 36);
+Person person2 = new("Andres2", 36);
 
-personStorage.Add(new Person("Andres", 36));
+personStorage.Add(person);
 personStorage.Add(person2);
+personStorage.DisplayItems();
+personStorage.Remove(person);
+personStorage.DisplayItems();
 
+DataStorage<Car> carStorage = new();
+Car car = new("Ferrari", "365");
+Car car2 = new("Tesla", "R2D2");
 
-Console.WriteLine(DataStorage<Person>.TotalItems);
-
+carStorage.Add(car);
+carStorage.Add(car2);
+carStorage.DisplayItems();
+carStorage.Remove(car);
+carStorage.DisplayItems();
