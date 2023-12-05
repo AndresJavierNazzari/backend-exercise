eba33ae32111aec9d2b229d93a998440558d626b
Andres Nazzari <lordcliff@gmail.com>
2023-12-05 22:10:49 +0900

    Create Category model

---
+++ wakuwakuApp/wakuwakuApi/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace wakuwakuApi.Entities {
+    public class Category {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Category(string name, string description) {
+            Name = name;
+            Description = description;
+        }
+    }
+}
