3c57e4d9ac53b9bf18649ef57a5ca013e8b330f0
Andres Nazzari <lordcliff@gmail.com>
2023-12-07 06:06:55 +0900

    Add key to ConnectionStrings config

---
+++ WakuwakuApp/wakuwakuApi/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Reflection;
+
+namespace wakuwakuApi {
+    public class Program {
+
+
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers().AddNewtonsoftJson();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if(app.Environment.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+
+    }
+}
+
+
+
+
