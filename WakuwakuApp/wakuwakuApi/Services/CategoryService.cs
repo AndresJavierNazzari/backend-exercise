1c68447f4a74eb383f975ad572fea39e9701189f
Andres Nazzari <lordcliff@gmail.com>
2023-12-08 05:45:53 +0900

    Create CategoryService

---
+++ WakuwakuApp/wakuwakuApi/Services/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using wakuwakuApi.Models;
+using wakuwakuApi.Repositories.Interfaces;
+using wakuwakuApi.Services.Interfaces;
+
+namespace wakuwakuApi.Services {
+    public class CategoryService : ICategoryService {
+
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository) {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<Category> GetCategories(int page, int pageSize, string filter) {
+            var result = _categoryRepository.GetCategories(page, pageSize, filter);
+            return result;
+        }
+
+    }
+}
