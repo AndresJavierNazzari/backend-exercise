6163fc800e0cd6190ca5536cd8b6323637def236
Andres Nazzari <lordcliff@gmail.com>
2023-12-08 05:45:19 +0900

    Create ICategoryRepository Interface

---
+++ WakuwakuApp/wakuwakuApi/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using wakuwakuApi.Models;
+
+namespace wakuwakuApi.Repositories.Interfaces {
+    public interface ICategoryRepository {
+
+        IEnumerable<Category> GetCategories(int page, int pageSize, string filter);
+
+    }
+}
