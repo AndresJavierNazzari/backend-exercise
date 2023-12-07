e521bdcfe7d4cd8cd3ef2723e3e4d5bab710940d
Andres Nazzari <lordcliff@gmail.com>
2023-12-08 05:45:41 +0900

    Create ICategoryService

---
+++ WakuwakuApp/wakuwakuApi/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using wakuwakuApi.Models;
+
+namespace wakuwakuApi.Services.Interfaces {
+    public interface ICategoryService {
+        IEnumerable<Category> GetCategories(int page, int pageSize, string filter);
+    }
+}
