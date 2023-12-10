d4aec8d0a855c0a9bd022c86c7c923d4d7d27d52
Andres Nazzari <andresnazzari@gmail.com>
2023-12-10 21:26:54 +0900

    Create GoalUpdate

---
+++ WakuwakuApp/wakuwakuApi/Models/GoalUpdate.cs
@@ -0,0 +1,8 @@
+﻿namespace wakuwakuApi.Models {
+    public class GoalUpdate {
+        public string Description { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public DateTime Deadline { get; set; }
+        public String Status { get; set; }
+    }
+}
