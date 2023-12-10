da842e3f92dfc66245325a92b15bebeb90757c48
Andres Nazzari <lordcliff@gmail.com>
2023-12-10 19:42:53 +0900

    Create category validator

---
+++ WakuwakuApp/wakuwakuApi/Validators/CategoryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using wakuwakuApi.Models;
+
+namespace wakuwakuApi.Validators {
+    public class CategoryValidator : AbstractValidator<Category> {
+        public CategoryValidator() {
+            RuleFor(c => c.Name).NotEmpty().MaximumLength(50).WithMessage("The maximum length is 50 characters.");
+            RuleFor(c => c.Description).NotEmpty().MaximumLength(200).WithMessage("The maximum length is 200 characters.");
+        }
+    }
+}
