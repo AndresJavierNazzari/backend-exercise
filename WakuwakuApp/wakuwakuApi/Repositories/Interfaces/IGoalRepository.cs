8c7e46d1181344f26961670ffb3c3d79399532dd
Andres Nazzari <andresnazzari@gmail.com>
2023-12-10 21:27:39 +0900

    Create IGoalRepository

---
+++ WakuwakuApp/wakuwakuApi/Repositories/Interfaces/IGoalRepository.cs
@@ -0,0 +1,13 @@
+﻿using wakuwakuApi.Models;
+
+namespace wakuwakuApi.Repositories.Interfaces;
+public interface IGoalRepository {
+
+    IEnumerable<Goal> GetGoals();
+    Goal GetGoalById(int goalId);
+    Goal AddGoal(Goal goal);
+    Goal UpdateGoal(int goalId, Goal goal);
+    Goal DeleteGoalById(int goalId);
+
+}
+
