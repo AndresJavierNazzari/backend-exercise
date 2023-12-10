caffceb4fe2da12e41f56a94aeb3b4b4af6ae8f0
Andres Nazzari <lordcliff@gmail.com>
2023-12-10 19:04:10 +0900

    Create Goal model

---
+++ WakuwakuApp/wakuwakuApi/Models/Goal.cs
@@ -0,0 +1,18 @@
+﻿namespace wakuwakuApi.Models {
+    public class Goal {
+        private static int lastId = 0;
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public DateTime Deadline { get; set; }
+        public String Status { get; set; }
+
+        public Goal(string description, DateTime dateOfCreation, DateTime deadline, string status) {
+            this.Id = ++lastId;
+            this.Description = description;
+            this.DateOfCreation = dateOfCreation;
+            this.Deadline = deadline;
+            this.Status = status;
+        }
+    }
+}
