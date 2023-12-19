8f2f4cb3b482510f77206f351404855d9ffd3afa
Andres J. Nazzari <andres.nazzari@jala.university>
2023-12-19 14:50:11 -0300

    Add class 21 documentation

---
+++ class21/pruebasDataTypes/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace pruebasDataTypes {
+    internal class Program {
+        static void Main(string[] args) {
+            RecordExample();
+            StructExample();
+        }
+
+        public static void StructExample() {
+
+
+        }
+
+        public struct AreaCuadrado {
+            public int Largo { get; }
+            public int Ancho { get; }
+
+            public AreaCuadrado(int largo, int ancho) {
+                Largo = largo;
+                Ancho = ancho;
+            }
+
+            public override string ToString() {
+                return $"El area del cuadrado es: ${Largo * Ancho}";
+            }
+        }
+
+
+
+
+        public static void RecordExample() {
+            Console.WriteLine("Record Example!");
+
+            Car car1 = new Car("Tesla");
+            Console.WriteLine("Auto 1: " + car1);
+            car1.Brand = "Ford";
+            Console.WriteLine("Persona C 1: " + car1);
+
+
+            Console.WriteLine("****** Record *******");
+            CarRecord car2 = new CarRecord("Ferrari");
+            //car2.Brand = "Honda";
+
+            Console.WriteLine("Persona R 1: " + car2);
+        }
+        public record CarRecord(string Brand);
+
+        public class Car {
+            public string Brand { get; set; }
+            public Car(string brand) {
+                Brand = brand;
+            }
+            public override string ToString() {
+                return $"Auto marca: {Brand}";
+            }
+        }
+
+    }
+}
